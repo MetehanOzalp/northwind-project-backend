17e24ff951a5537d2ebb4b60d0f81f52a92e5818
Metehan Özalp <metehanozalp@hotmail.com>
2021-05-28 14:52:58 +0300

    product queries and ORM added

--- src/main/java/CampProject/northwind/dataAccess/abstracts/ProductDao.java
+++ src/main/java/CampProject/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,27 @@
 package CampProject.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import CampProject.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
+
+	Product getByProductName(String name);
+
+	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
+
+	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+
+	List<Product> getByCategoryIn(int... categories);
+
+	List<Product> getByProductNameContains(String productName);
+
+	List<Product> getByProductNameStartsWith(String productName);
+
+	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
+	List<Product> getByNameAndCategory(String productName, int categoryId);
 
 }
