231ad59444826a5318506d6fc8564f0ec2bba328
Metehan Özalp <metehanozalp@hotmail.com>
2021-06-06 16:41:22 +0300

    added cross origin

--- src/main/java/CampProject/northwind/api/controllers/UsersController.java
+++ src/main/java/CampProject/northwind/api/controllers/UsersController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import CampProject.northwind.core.utilities.results.ErrorDataResult;
 
 @RestController
 @RequestMapping("api/users/")
+@CrossOrigin
 public class UsersController {
 
 	private UserService userService;
